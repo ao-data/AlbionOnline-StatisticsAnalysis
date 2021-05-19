c05f6047bd8a9d8545856d364f8dca2ad69401ae
Aaron Schultz <schultzaaron@web.de>
2021-05-19 14:07:39 +0200

    Added CraftingInfo class and Artifacts enum

---
+++ StatisticsAnalysisTool/StatisticsAnalysisTool/Enumerations/Artifacts.cs
@@ -0,0 +1,11 @@
+﻿namespace StatisticsAnalysisTool.Enumerations
+{
+    public enum Artifact
+    {
+        Unknown = -1,
+        Runes = 0,
+        Souls = 1,
+        Relics = 2,
+        Avalon = 3
+    }
+}
---
+++ StatisticsAnalysisTool/StatisticsAnalysisTool/Models/CraftingInformation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace StatisticsAnalysisTool.Models
+{
+    public class CraftingInformation
+    {
+        public ObservableCollection<CraftResource> CraftResource { get; set; }
+
+        public int TotalAmountResources => CraftResource?.Sum(x => x?.Count) ?? 0;
+
+        public int CraftingJournalFillRate => 100;
+    }
+}
